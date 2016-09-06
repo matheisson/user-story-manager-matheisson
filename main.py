ab7fe1f6f5bfc57a20b10ef01798f2979cccb460
matheisson <csanyil@gmail.com>
2016-09-06 17:06:08 +0200

    Server runs, everything works, but no bin/pencil icon

--- main.py
+++ main.py
@@ -2,7 +2,7 @@ from peewee import *
 from flask import Flask, request, url_for, redirect, render_template
 
 
-DATABASE =  'userstory.db'
+DATABASE = 'userstory.db'
 DEBUG = True
 SECRET_KEY = 'verysecretkey'
 
@@ -56,7 +56,7 @@ def story_page():
     if request.method == "POST": #executes the query if we want to submit a user story. on the right of the = are the corresponding HTML names
         UserStory.create(story_title = request.form['story_title'], user_story=request.form['user_story'], criteria=request.form['criteria'],
                           b_value=request.form['b_value'], estimation=request.form['estimation'], status=request.form['status'])
-        return redirect(url_for('homepage'))
+        return redirect(url_for('story_list'))
     else: # show the prev. sent data
         return render_template('form.html', prev_data='', submit='Create')
 
@@ -64,7 +64,7 @@ def story_page():
 @app.route('/story/<story_id>', methods=['GET', 'POST'])
 def update_story(story_id):
     if request.method == 'POST':
-        UserStory.update(story_title = request.form['story_title'], user_story=request.form['user_story'], criteria=request.form['criteria'],
+        UserStory.update(story_title=request.form['story_title'], user_story=request.form['user_story'], criteria=request.form['criteria'],
                          b_value=request.form['b_value'], estimation=request.form['estimation'],
                          status=request.form['status']).where(UserStory.id == story_id).execute()
         return redirect(url_for('homepage'))
